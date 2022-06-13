7ecdd06ca0e75d77d3cb8b588701ccc75002452b
JMSEhsan@gmail.com <Ehsan Jalali>
2022-06-12 23:21:34 -0600

    Commit: initialize Adjacency Matrix

---
+++ AdjacencyMatrix.py
@@ -0,0 +1,17 @@
+# Plot adjacency matrix
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+matrix = np.array([[0,0,0,1,0]
+                   ,[0,0,0,1,1]
+                   ,[0,1,0,0,1]
+                   ,[1,1,0,0,1]
+                   ,[0,1,1,1,0]])
+
+G = nx.from_numpy_array(matrix)
+
+nx.draw(G, with_labels=True)
+plt.show()
